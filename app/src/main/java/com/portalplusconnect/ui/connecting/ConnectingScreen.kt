940b42fde274c0566357e00758711b10d11ffed9
leandrocrhe <108886070+leandrocrhe@users.noreply.github.com>
2026-08-02 12:07:54 -0400

    feat: add connecting compose screen

---
+++ app/src/main/java/com/portalplusconnect/ui/connecting/ConnectingScreen.kt
@@ -0,0 +1,9 @@
+package com.portalplusconnect.ui.connecting
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ConnectingScreen() {
+    Text("PortalPlus Connect\n\nConectando...\n\nVerificando red Wi-Fi")
+}
